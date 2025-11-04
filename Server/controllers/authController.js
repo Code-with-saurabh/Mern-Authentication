e6ced67889243409f42b40c41e5a1e7f6e566c40
Saurabh Sharma <idkxyz001@gmail.com>
2025-11-04 18:36:36 +0530

    create a logout controller function

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -110,5 +110,26 @@ const login = async(req,res)=>{
 }
 
 
+const logout = async (req,res)=>{
+    try{
+        res.clearCookie('token',{
+            httpOnly:true,
+            secure:NODE_ENV==="production",
+            sameSite:NODE_ENV==="production"?'none':'strict',
+            maxAge:7*24*60*60*1000,
+        })
 
-module.exports = {register}
+        return res.json({
+            success:true,
+            message:"Logged out..."
+        })
+    }catch(error){
+        res.json({
+                success:false,
+                message:error.message
+            })
+    }
+}
+
+
+module.exports = {register,login,logout}
